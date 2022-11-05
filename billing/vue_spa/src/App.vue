60a72bcb9a422c5b06404c332adf7c351a636b15
Jane49-cloud <janendirangu49@gmail.com>
2022-11-05 09:10:28 +0300

    Billing App frontend with Vue.js

---
+++ billing/vue_spa/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="app">
+      <InvoiceCreate/>
+        </div>
+</template>
+
+<script>
+import InvoiceCreate from "./components/InvoiceCreate.vue";
+export default{
+  name : "App",
+  components:{
+      InvoiceCreate
+  }
+}
+</script>
---
+++ billing/vue_spa/vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports = {
+ publicPath: process.env.VUE_APP_STATIC_URL,
+ outputDir: path.resolve(__dirname, "../static", "billing"),
+indexPath: path.resolve(__dirname, "../templates/", "billing",
+"index.html")
+};
